596966bfa442d107a6a469b5322e32ad67d99743
ashreekanth <ashreekanth@vmware.com>
2020-06-18 00:32:56 -0700

    feature commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React the right way with Akshay. Hello all
+          I was changed on feature branch
         </a>
       </header>
     </div>
